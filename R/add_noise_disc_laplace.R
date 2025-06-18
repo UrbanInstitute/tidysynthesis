291aac296ab23199fb7cac6cd7843091f4df0f0e
Jeremy Seeman <143003789+jhseeman@users.noreply.github.com>
2025-06-18 08:25:38 -0400

    add discrete noise mechanisms

---
+++ R/add_noise_disc_laplace.R
@@ -0,0 +1,85 @@
+#' 
+#' Add discrete Laplace noise with mean 0 to predicted values with constant variance
+#'
+#' @param model A `model_spec` or a list of `model_spec`s from `library(parsnip)`
+#' @param new_data A data frame used to generate predictions
+#' @param conf_model_data A data frame for estimating the predictive model
+#' @param outcome_var A string name representing the outcome variable
+#' @param col_schema A list of column schema specifications for the new variable
+#' @param pred A vector of values predicted by the model
+#' @param variance Sampling variance for additive noise
+#' @param epsilon Alternative privacy loss budget prescribed by the Laplace
+#' mechanism under epsilon differential privacy.
+#' @param sensitivity Alternative sample sensitivity prescribed by the Laplace
+#' mechanism under epsilon differential privacy.
+#' @param increment Numeric indicating space between discrete noise samples, 
+#' defaults to 1. Note that this does not impact the noise sampling variance, as 
+#' the increment rescales noise distributions specified by sampling variance.
+#'
+#' @return A numeric vector with noise added to each prediction
+#' 
+#' @export
+#' 
+add_noise_disc_laplace <- function(
+    model,
+    new_data,
+    conf_model_data,
+    outcome_var,
+    col_schema,
+    pred,
+    variance = NULL,
+    epsilon = NULL,
+    sensitivity = NULL,
+    increment = 1) {
+  
+  stopifnot("add_noise_disc_laplace increment must be greater than 0." = { 
+    increment > 0 
+  })
+  
+  # if variance directly specified...
+  if (!is.null(variance)) {
+    
+    if (!is.null(epsilon) | !is.null(sensitivity)) {
+      
+      stop("Cannot use non-null variance with non-null epsilon or sensitivity.")
+      
+    }
+    
+    stopifnot(is.numeric(variance))
+    stopifnot(variance > 0)
+    
+    # for different increments, rescale to normalize by the increment
+    scale1_var <- variance / (increment**2)
+    
+    # inverse of discrete laplace variance as a function of scale param...
+    scale_param <- log(1 + (1. + sqrt(2 * scale1_var + 1)) / scale1_var)
+    
+    # else if using epsilon-DP Laplace mechanism
+  } else {
+    
+    if (is.null(epsilon) | is.null(sensitivity)) {
+      
+      stop("Must specify either `variance` or both `epsilon` and `sensitivity`.")
+      
+    }
+    
+    stopifnot(is.numeric(sensitivity))
+    stopifnot(sensitivity > 0)
+    stopifnot(is.numeric(epsilon))
+    stopifnot(epsilon > 0)
+    
+    scale_param <- epsilon / sensitivity 
+    
+  }
+  
+  # compute scale=1 noise 
+  p <- 1 - exp(-scale_param)
+  d1 <- stats::rgeom(n = length(pred), p = p)
+  d2 <- stats::rgeom(n = length(pred), p = p)
+  scale1_noise <- d1 - d2
+  
+  # add noise to final result and return
+  noisy_result <- pred + increment * scale1_noise
+  return(noisy_result)
+  
+}
---
+++ tests/testthat/test-add_noise_disc_gaussian.R
@@ -0,0 +1,141 @@
+# specify default arguments for add_noise_disc_gaussian
+# note that NULL values may be supplied to the function and left unused
+model <- NULL
+new_data <- NULL
+conf_model_data <- NULL
+outcome_var <- NULL
+col_schema <- NULL
+pred <- 1:100
+
+test_that("add_noise_disc_gaussian reproduces noise", {
+  
+  set.seed(1)
+  noisy_preds1 <- add_noise_disc_gaussian(
+    model = model,
+    new_data = new_data,
+    conf_model_data,
+    outcome_var = outcome_var,
+    col_schema = col_schema,
+    pred = pred,
+    variance = 3
+  )
+  
+  set.seed(1)
+  noisy_preds2 <- add_noise_disc_gaussian(
+    model = model,
+    new_data = new_data,
+    conf_model_data,
+    outcome_var = outcome_var,
+    col_schema = col_schema,
+    pred = pred,
+    variance = 3
+  )
+  
+  expect_equal(noisy_preds1, noisy_preds2)
+  
+})
+
+test_that("add_noise_disc_gaussian reproduces with either direct var or zCDP", {
+  
+  set.seed(1)
+  noisy_preds1 <- add_noise_disc_gaussian(
+    model = model,
+    new_data = new_data,
+    conf_model_data,
+    outcome_var = outcome_var,
+    col_schema = col_schema,
+    pred = pred,
+    variance = 2
+  )
+  
+  set.seed(1)
+  noisy_preds2 <- add_noise_disc_gaussian(
+    model = model,
+    new_data = new_data,
+    conf_model_data,
+    outcome_var = outcome_var,
+    col_schema = col_schema,
+    pred = pred,
+    rho = 1,
+    sensitivity = 2
+  )
+  
+  expect_equal(noisy_preds1, noisy_preds2)
+  
+})
+
+test_that("add_noise_disc_laplace increment rescaling", {
+  
+  set.seed(1)
+  noisy_preds1 <- add_noise_disc_gaussian(
+    model = model,
+    new_data = new_data,
+    conf_model_data,
+    outcome_var = outcome_var,
+    col_schema = col_schema,
+    pred = pred,
+    variance = 1,
+    increment = 5
+  )
+  
+  # all noise increments should be 0 modulo 5 (the increment)
+  expect_true(all((noisy_preds1 - pred) %% 5 == 0))
+  
+})
+
+test_that("add_noise_disc_gaussian error handling", {
+  
+  # no variance parameters specified
+  expect_error(
+    add_noise_disc_gaussian(
+      model = model,
+      new_data = new_data,
+      conf_model_data,
+      outcome_var = outcome_var,
+      col_schema = col_schema,
+      pred = pred
+    )
+  )
+  
+  # too many variance parameters specified
+  expect_error(
+    add_noise_disc_gaussian(
+      model = model,
+      new_data = new_data,
+      conf_model_data,
+      outcome_var = outcome_var,
+      col_schema = col_schema,
+      pred = pred,
+      variance = 1,
+      rho = 2
+    )
+  )
+  
+  # not enough variance parameters specified
+  expect_error(
+    add_noise_disc_gaussian(
+      model = model,
+      new_data = new_data,
+      conf_model_data,
+      outcome_var = outcome_var,
+      col_schema = col_schema,
+      pred = pred,
+      rho = 2
+    )
+  )
+  
+  # incorrect increment 
+  expect_error(
+    add_noise_disc_gaussian(
+      model = model,
+      new_data = new_data,
+      conf_model_data,
+      outcome_var = outcome_var,
+      col_schema = col_schema,
+      pred = pred,
+      variance = 1,
+      increment = 0
+    )
+  )
+  
+})
